acab4353dd6298af23989ab728c2755e8b3ff15e
GovindSingh3011 <govindrawat3011@gmail.com>
2025-11-03 19:34:21 +0530

    feat: add route for retrieving archived logs in project controller

--- Backend-Server/routes/project.js
+++ Backend-Server/routes/project.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { createProject, streamLogs } = require('../controllers/projectController');
+const { createProject, streamLogs, getArchivedLogs } = require('../controllers/projectController');
 
 router.post('/', createProject);
 router.get('/:projectId/logs/stream', streamLogs);
+router.get('/:projectId/logs/archive', getArchivedLogs);
 
 module.exports = router;
