b366651f53ef2e1b17602dd3164454acabd6bf1d
GovindSingh3011 <govindrawat3011@gmail.com>
2025-11-18 13:33:48 +0530

    feat: enhance reverse proxy with custom 404 handling and response management

--- Reverse-Proxy/index.js
+++ Reverse-Proxy/index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const httpProxy = require('http-proxy')
+const fs = require('fs')
+const path = require('path')
 require('dotenv').config()
 
 const app = express()
@@ -8,23 +10,50 @@ const BASE_PATH = process.env.BASE_PATH
 
 const proxy = httpProxy.createProxy()
 
+const custom404Page = fs.readFileSync(path.join(__dirname, '404.html'), 'utf8')
+
 app.use((req, res) => {
     const hostname = req.hostname;
     const subdomain = hostname.split('.')[0];
-
     const resolvesTo = `${BASE_PATH}/${subdomain}`;
-    
+
     console.log('Hostname:', hostname);
     console.log('Subdomain:', subdomain);
     console.log('Proxying request to:', resolvesTo);
 
-    return proxy.web(req, res, { target: resolvesTo, changeOrigin: true });
-})
+    proxy.web(req, res, { target: resolvesTo, changeOrigin: true, selfHandleResponse: true });
+});
+
+proxy.on('proxyRes', (proxyRes, req, res) => {
+    let body = [];
+    
+    proxyRes.on('data', function(chunk) {
+        body.push(chunk);
+    });
+    
+    proxyRes.on('end', function() {
+        body = Buffer.concat(body).toString();
+        
+        if (body.includes('<Code>AccessDenied</Code>') || body.includes('<Code>NoSuchKey</Code>')) {
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            res.end(custom404Page);
+        } else {
+            res.writeHead(proxyRes.statusCode, proxyRes.headers);
+            res.end(body);
+        }
+    });
+});
 
 proxy.on('proxyReq', (proxyReq, req, res) => {
     const url = req.url;
     if (url === '/')
-        proxyReq.path += 'index.html'
-})
+        proxyReq.path += 'index.html';
+});
+
+proxy.on('error', (err, req, res) => {
+    console.error('Proxy error:', err);
+    res.writeHead(404, { 'Content-Type': 'text/html' });
+    res.end(custom404Page);
+});
 
 app.listen(PORT, () => console.log(`Reverse Proxy Running on port ${PORT}`))
