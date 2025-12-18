ca9b31da921d39717f2d338dc2bb2a7cc05f426b
GovindSingh3011 <govindrawat3011@gmail.com>
2025-12-18 21:30:02 +0530

    feat(App): add routes for Projects and Project components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import Projects from './pages/Projects'
+import Project from './pages/Project'
 import Auth from './pages/Auth'
 import './App.css'
 
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/auth" element={<Auth />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/projects/:projectId" element={<Project />} />
       </Routes>
     </Router>
   )
