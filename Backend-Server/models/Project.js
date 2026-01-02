d63aaa5fd0a4618850e2625a9d4af94e86091101
GovindSingh3011 <govindrawat3011@gmail.com>
2026-01-02 11:43:56 +0530

    feat(Project): add autoRedeploy and buildConfig fields to project schema

--- Backend-Server/models/Project.js
+++ Backend-Server/models/Project.js
@@ -28,6 +28,36 @@ const projectSchema = new mongoose.Schema({
         default: null,
         trim: true,
     },
+    autoRedeploy: {
+        type: Boolean,
+        default: false,
+    },
+    webhookSecret: {
+        type: String,
+        default: null,
+    },
+    webhookId: {
+        type: String,
+        default: null,
+    },
+    lastCommitHash: {
+        type: String,
+        default: null,
+    },
+    buildConfig: {
+        installCmd: {
+            type: String,
+            default: 'npm install',
+        },
+        buildCmd: {
+            type: String,
+            default: 'npm run build',
+        },
+        buildRoot: {
+            type: String,
+            default: null,
+        },
+    },
 }, { timestamps: true });
 
 module.exports = mongoose.model('Project', projectSchema);
